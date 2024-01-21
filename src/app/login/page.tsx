16c74db597746a655a34b75a44887ff90437b8bc
Mahendra <mahendrakondadasu@gmail.com>
2024-01-21 16:40:41 +0530

    fix: remove unnecessary header values.

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,8 +2,6 @@ export default function LoginPage() {
     return (
         <div>
             <h1>Login</h1>
-            <h2>Second login page</h2>
-            <h3>h3</h3>
         </div>
     );
 }
